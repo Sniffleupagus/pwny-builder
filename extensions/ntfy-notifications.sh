39f7b80fd02fed6d610b431859e17f31df915dc3
Sniffleupagus <midden_sofas0k@icloud.com>
2024-12-11 01:09:31 +0000

    improve ntfy integration and better reporting

--- extensions/ntfy-notifications.sh
+++ extensions/ntfy-notifications.sh
@@ -1,35 +1,39 @@
 #
 # send notifications via ntfy.sh at different points along the build
 #
+NTFY_SERVER=${NTFY_SERVER:-"ntfy.sh"}
+NTFY_TOPIC=${NTFY_TOPIC:-""}
 
 function ntfy_send() {
-    msg=${1:-"Nothing happened."}
-    title=${2:-''}
-    priority=${3:-'default'}
-    tags=${4:-'warning'}
-    markdown=${5:-''}
-    topic=${6:-'pwny_builder'}
-    
-    echo "${msg}" | curl -s -H "t:${title}"  -H "p:${priority}" -H "ta: ${tags}" -d @-  ntfy.sh/${topic}
+    title=${1:-''}
+    priority=${2:-'default'}
+    tags=${3:-'warning'}
+    markdown=${4:-''}
+
+    if [ -n "${NTFY_TOPIC}" ]; then
+	cat | curl -s -H "t:${title}"  -H "p:${priority}" -H "ta: ${tags}" -d @-  ${NTFY_SERVER}/${NTFY_TOPIC}
+    else
+	cat >/dev/null
+    fi
 }
 
 function post_family_config__ntfy_send() {
-    ntfy_send "Family configured. Let's go!" "PwnyBuild ${BOARD}" default partying_face
+    pwd | ntfy_send "Starting ${BOARD}" default partying_face
 }
 
 function pre_customize_image__ntfy_send() {
-    ntfy_send "Beginning pwnagotchi customzation" "PwnyBuild ${BOARD}" default toolbox
+    ls -latr ${SDCARD}/tmp | ntfy_send "You like a ${BOARD} on yo ${BOARD}?" 2 toolbox
 }
 
 function post_customize_image__ntfy_send() {
-    ntfy_send "Finished pwnagotchi customization" "PwnyBuild ${BOARD}" default vulcan_salute
+    ls -a ${SDCARD}/{etc,home}/pwnagotchi | ntfy_send "Now you got a ${BOARD} on yo ${BOARD}!" 2 vulcan_salute
 }
 
 function post_create_partitions__ntfy_send() {
-    ntfy_send "$(sfdisk -l ${SDCARD}.raw)" "${BOARD} Partitions complete" default card_index_dividers
+    sfdisk -l ${SDCARD}.raw | ntfy_send "${BOARD} Partitions complete" 2 card_index_dividers
 }
 
 function run_after_build__ntfy_send() {
-    ntfy_send "Build completed at $(date)" "PwnyBuild ${BOARD}" default checkered_flag
+    ls -ltr output/images | tail -5 | ntfy_send "${BOARD} Completed" default checkered_flag
 }
 
