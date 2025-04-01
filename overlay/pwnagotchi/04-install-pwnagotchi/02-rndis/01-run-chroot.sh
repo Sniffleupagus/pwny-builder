151507ad38e00f5f365deafebe76a4aff9fd3fec
Sniffleupagus <midden_sofas0k@icloud.com>
2025-04-01 06:16:01 +0000

    check for 'nmcli conn up' before adding. g_ether->g_cdc

--- overlay/pwnagotchi/04-install-pwnagotchi/02-rndis/01-run-chroot.sh
+++ overlay/pwnagotchi/04-install-pwnagotchi/02-rndis/01-run-chroot.sh
@@ -10,7 +10,7 @@ echo "*** Configuring RNDIS"
 
 # enable usb0 via nmcli
 for rc in rc.local rc.local.ORIG; do
-    if [ -f /etc/${rc} ]; then
+    if ! grep 'nmcli conn up usb0' /etc/${rc} ; then
 	echo "|-+ bring usb0/RNDIS up in ${rc}"
 	sed -i '/^exit 0/inmcli conn up usb0' /etc/${rc}
     fi
@@ -28,10 +28,10 @@ if [[ "${BOARD}" == "bananapim4zero" ]]; then
     fi
 fi
 
-# set host and dev ID on g_ether, so not random RNDIS devices
-if [ ! -f /etc/modprobe.d/g_ether.conf ]; then
-   echo "|-> Setting up g_ether device IDs"
-   echo "options g_ether use_eem=0 host_addr=f0:0d:ba:be:f0:0d dev_addr=58:70:77:6e:79:58" > /etc/modprobe.d/g_ether.conf
+# set host and dev ID on g_cdc, so not random RNDIS devices
+if [ ! -f /etc/modprobe.d/g_cdc.conf ]; then
+   echo "|-> Setting up g_cdc device IDs"
+   echo "options g_cdc use_eem=0 host_addr=f0:0d:ba:be:f0:0d dev_addr=58:70:77:6e:79:58" > /etc/modprobe.d/g_ether.conf
 fi
 
 echo "|-- RNDIS Configuration Complete"
