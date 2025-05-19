dde78491fad2c07ebaed9df7f8c30a0e66521ea2
Sniffleupagus <midden_sofas0k@icloud.com>
2025-05-18 18:57:31 -0700

    removed unecessary check

--- overlay/pwnagotchi/01-config-user/01-run-chroot.sh
+++ overlay/pwnagotchi/01-config-user/01-run-chroot.sh
@@ -7,8 +7,6 @@ PHOME=/home/${PUSER}
 PVENV=${PHOME}/.venv
 PGROUPS=adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,render,netdev,i2c
 
-groups pwnagotchi || true
-
 id -u pwnagotchi || useradd -c "Pwnagotchi" -p $(echo pwny1234 | openssl passwd -1 -stdin) -G ${PGROUPS} -d ${PHOME} -m ${PUSER} -k /etc/skel -s /bin/bash
 
 if ! grep "pwnagotchi addons" ${PHOME}/.bashrc ; then
