a5adb88f99dcc2b4d2fbb014b8131ace991d6983
Sniffleupagus <midden_sofas0k@icloud.com>
2025-05-23 13:12:00 -0700

    remove artifacts from image after saving back to host side

--- overlay/pwnagotchi/06-save-artifacts/02-run.sh
+++ overlay/pwnagotchi/06-save-artifacts/02-run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash -e
 
+figlet "artifacts"
+
+echo "Rootfs: ${ROOTFS_DIR}, pwd: $(pwd)"
+ls -l ${ROOTFS_DIR}/root
+
 if [ "${ROOTFS_DIR}" -a "${ROOTFS_DIR}" != "/" -a -d ${ROOTFS_DIR}/root/artifacts ]; then
     figlet "Save Artifacts"
     cd ..
+    echo "+ saving to $(pwd)"
     mkdir -p artifacts
     rsync -av ${ROOTFS_DIR}/root/artifacts/ artifacts/
+    rm -r ${ROOTFS_DIR}/root/artifacts
 fi
 
