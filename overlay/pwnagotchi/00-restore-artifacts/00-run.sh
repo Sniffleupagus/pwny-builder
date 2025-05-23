64428a858b87f3c265f5584befd52dbe27ebeda1
Sniffleupagus <midden_sofas0k@icloud.com>
2025-05-23 13:08:30 -0700

    redefine OVERLAY_DIR to include /pwnagotchi

--- overlay/pwnagotchi/00-restore-artifacts/00-run.sh
+++ overlay/pwnagotchi/00-restore-artifacts/00-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+
+pwd
+
 source ../common.sh
 
 #
@@ -13,19 +16,22 @@ source ../common.sh
 #   so using /root/overlay/pwnagotchi instead
 
 echo "Starting in $(pwd). Root dir: ${ROOTFS_DIR}"
-echo "${OVERLAY_DIR}:"
-
-if [ ! -d ${ROOTFS_DIR}${OVERLAY_DIR}/pwnagotchi ]; then
-    echo "+ Creating ${OVERLAY_DIR}/pwnagotchi"
-    mkdir -p ${ROOTFS_DIR}${OVERLAY_DIR}/pwnagotchi
-else
-    ls -l ${ROOTFS_DIR}${OVERLAY_DIR}/pwnagotchi
-fi
+echo "Overlay: ${OVERLAY_DIR}"
+echo "SDCARD: ${SDCARD}"
+echo "AS: ${PWNY_ARTIFACT_SUB}"
 
 if [ "${ROOTFS_DIR}" != "" -a "${ROOTFS_DIR}" != "/" ]; then
+    if [ ! -d ${ROOTFS_DIR}${OVERLAY_DIR} ]; then
+	echo "+ Creating ${OVERLAY_DIR}"
+	mkdir -p ${ROOTFS_DIR}${OVERLAY_DIR}
+    else
+	ls -l ${ROOTFS_DIR}${OVERLAY_DIR}
+    fi
+
     pushd ..
     echo "+ Copying pwnagotchi overlay to ${OVERLAY_DIR}"
-    rsync --exclude '*~' --exclude '.#*' --exclude SKIP --exclude SKIP_IMAGES --exclude EXPORT_IMAGE -avz . ${ROOTFS_DIR}${OVERLAY_DIR}/pwnagotchi/
+    rsync --exclude '*~' --exclude '.#*' --exclude SKIP --exclude SKIP_IMAGES --exclude EXPORT_IMAGE -avz . ${ROOTFS_DIR}${OVERLAY_DIR}/
     popd
+else
+    echo Not copying overlay
 fi
-
