5d3fc22993e9d910efc825496ce09539ec3c3d48
Sniffleupagus <midden_sofas0k@icloud.com>
2025-05-23 13:06:51 -0700

    determine artifact dir based on builder

--- overlay/pwnagotchi/common.sh
+++ overlay/pwnagotchi/common.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
 
-export PWNY_BUILD_ARTIFACTS="/root/artifacts/${PI_GEN}/${ARCH}/${RELEASE}/${IMG_NAME}"
-export OVERLAY_DIR=${OVERLAY_DIR:-/root/overlay}
+export PWNY_BUILD_ARTIFACTS=${PWNY_BUILD_ARTIFACTS:-"/root/artifacts/${PI_GEN}/${ARCH}/${RELEASE}/${IMG_NAME}"}
+export OVERLAY_DIR=${OVERLAY_DIR:-"/root/overlay/pwnagotchi"}
+
+case "${PI_GEN}" in
+    "pi-gen")
+	export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${ARCH}/${RELEASE}/${IMG_NAME}"} ;;
+    "armbian-build")
+	export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${BOARD}/${RELEASE}"} ;;
+    "deb-img-builder")
+	export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${BOARD}/${HOSTNAME}"} ;;
+    *)
+	export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/unknown"};;
+esac
 
 save_pwny_artifact () {
     file=$1
@@ -11,16 +22,71 @@ save_pwny_artifact () {
 	rm ${PWNY_BUILD_ARTIFACTS}/$dir/$fname
     fi
     
-    echo "Saving artifact ${file} to ${dir}"
+    echo "+ Saving artifact ${file} to ${dir}"
     mkdir -p ${PWNY_BUILD_ARTIFACTS}/${dir}
     cp ${file} ${PWNY_BUILD_ARTIFACTS}/${dir}
 }
 
 restore_pwny_artifacts () {
-    if pushd "${PWNY_BUILD_ARTIFACTS}/$1"; then
-	tar -cf - . | tar -C / -xvf -
+    echo "Looking for $1"
+    case "${PI_GEN}" in
+	"pi-gen")
+	    export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${ARCH}/${RELEASE}/${IMG_NAME}"} ;;
+	"armbian-build")
+	    export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${BOARD}/${RELEASE}"} ;;
+	"deb-img-builder")
+	    export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/${BOARD}/${HOSTNAME}"} ;;
+	*)
+	    export PWNY_ARTIFACT_SUB=${PWNY_ARTIFACT_SUB:-"${PI_GEN}/unknown"};;
+    esac
+    if pushd ${OVERLAY_DIR}/artifacts/${PWNY_ARTIFACT_SUB}/$1; then
+	#tar -cf - . | tar -C / -xvf -
+	#cp -rp ./* /
+	#rsync -av . /
+	for f in $(find . -type f); do
+	    echo "Installing ${PWNY_ARTIFACT_SUB}/$f"
+	    mkdir -p /$(dirname $f)
+	    cp $f /$f
+	done
 	popd
 	return 0
+    elif pushd "${PWNY_BUILD_ARTIFACTS}/$1"; then
+	#tar -cf - . | tar -C / -xvf -
+	#cp -rp ./* /
+	for f in $(find . -type f); do
+	    echo "Installing ${PWNY_ARTIFACT_SUB}/$f"
+	    mkdir -p /$(dirname $f)
+	    cp $f /$f
+	done
+	popd
+	return 0
+    elif pushd "/tmp/overlay/pwnagotchi/artifacts/${PWNY_ARTIFACT_SUB}/$1"; then
+	# armbian-build location
+	#tar -cf - . | tar -C / -xvf -
+	#cp -rp ./* /
+	for f in $(find . -type f); do
+	    echo "Installing ${PWNY_ARTIFACT_SUB}/$f"
+	    mkdir -p /$(dirname $f)
+	    cp $f /$f
+	done
+	popd
+	return 0
+    elif pushd "/root/artifacts/${PWNY_ARTIFACT_SUB}/$1"; then
+	# debian-image-builder
+	#tar -cf - . | tar -C / -xvf -
+	#cp -rp ./* /
+	for f in $(find . -type f); do
+	    echo "Installing ${PWNY_ARTIFACT_SUB}/$f"
+	    mkdir -p /$(dirname $f)
+	    cp $f /$f
+	done
+	popd
+	return 0
+    else
+	echo "! no artifact for $1"
+	return 1
     fi
+    false
     return 1
 }
+
