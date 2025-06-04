48be594df6630e94f7d0cbb6dabe323517ecc070
Sniffleupagus <midden_sofas0k@icloud.com>
2025-06-04 21:21:16 +0000

    fix ] in if

--- overlay/pwnagotchi/03-build-torch/00-run.sh
+++ overlay/pwnagotchi/03-build-torch/00-run.sh
@@ -2,7 +2,7 @@
 
 echo "Copying files from $(pwd)"
 
-if [ -f torch-packages]; then
+if [ -f torch-packages ]; then
     ls -l torch-packages
     cp torch-packages ${ROOTFS_DIR}/tmp/torch-packages
 fi
